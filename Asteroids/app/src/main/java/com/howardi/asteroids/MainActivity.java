f8835b7dd34d2933dbe23c875c26105bcf9568aa
Isaiah Howard <>
2020-06-05 16:41:37 +1000

    Add some more basic UI

--- Asteroids/app/src/main/java/com/howardi/asteroids/MainActivity.java
+++ Asteroids/app/src/main/java/com/howardi/asteroids/MainActivity.java
@@ -2,15 +2,19 @@ package com.howardi.asteroids;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
     }
 
