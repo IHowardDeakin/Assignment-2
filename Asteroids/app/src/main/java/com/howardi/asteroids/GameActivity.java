32304580201af18fbbd863f50f73e32109eb6b6e
Isaiah Howard <>
2020-06-05 16:09:09 +1000

    Force landscape mode

--- Asteroids/app/src/main/java/com/howardi/asteroids/GameActivity.java
+++ Asteroids/app/src/main/java/com/howardi/asteroids/GameActivity.java
@@ -1,5 +1,7 @@
 package com.howardi.asteroids;
 
+import android.annotation.SuppressLint;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 
@@ -7,9 +9,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class GameActivity extends AppCompatActivity {
 
+    @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_IMMERSIVE
